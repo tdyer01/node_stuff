c848a18f219a6314c37b4d4078ba99b825d7b1a6
Ted <tdyer0001@gmail.com>
2022-07-26 23:30:04 -0500

    test

--- dns.js
+++ dns.js
@@ -1,10 +1,10 @@
 var dns = require('dns');
 
-var w3 = dns.lookup('tantalum', function (err, addresses, family) {
+var w3 = dns.lookup('tellurium', function (err, addresses, family) {
     console.log(addresses);
   });
 
-  var w3 = dns.reverse('192.168.1.131', function (err, addresses, family) {
+  var w3 = dns.reverse('192.168.1.114', function (err, addresses, family) {
     console.log(addresses);
   });
 
